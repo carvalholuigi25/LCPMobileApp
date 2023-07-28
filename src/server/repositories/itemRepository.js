ac5b35c8b76d67ccc57ede12b052b18f5812a8dc
Luigi Carvalho <carvalholuigi25@gmail.com>
2023-07-28 15:57:39 +0100

    implemented the create, update and delete functions for item controllers, repos and services

--- src/server/repositories/itemRepository.js
+++ src/server/repositories/itemRepository.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import db from '../database/db';
 
 export const getAllItems = () => {
@@ -13,5 +14,69 @@ export const getAllItems = () => {
   });
 };
 
-// Add more methods for create, update, and delete operations
+export const getAllItemsById = (mid) => {
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'SELECT * FROM items WHERE id = ?',
+        [mid],
+        (_, { rows }) => resolve(rows._array),
+        (_, error) => reject(error),
+      );
+    });
+  });
+};
+
+export const insertItems = (name, desc) => {
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'INSERT INTO items (name, description) VALUES (?,?)',
+        [name, desc],
+        (_, { rows }) => resolve(rows._array),
+        (_, error) => reject(error),
+      );
+    });
+  });
+};
+
+export const updateItems = (mid, name, desc) => {
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'UPDATE items SET name = ?, description = ? WHERE id = ?',
+        [name, desc, mid],
+        (_, { rows }) => resolve(rows._array),
+        (_, error) => reject(error),
+      );
+    });
+  });
+};
+
+export const deleteItems = (mid) => {
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'DELETE FROM items WHERE id = ?',
+        [mid],
+        (_, { rows }) => resolve(rows._array),
+        (_, error) => reject(error),
+      );
+    });
+  });
+};
+
+export const deleteAllItems = () => {
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'DELETE FROM items',
+        [],
+        (_, { rows }) => resolve(rows._array),
+        (_, error) => reject(error),
+      );
+    });
+  });
+};
+
 // src: https://chat.openai.com/share/dce203e7-1502-4dae-b708-7bbf49142100
