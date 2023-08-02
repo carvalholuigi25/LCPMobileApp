9f70e60594511f91aa92fd7048448747b732d3a4
Luigi Carvalho <carvalholuigi25@gmail.com>
2023-08-02 15:23:19 +0100

    added language and theme pickers to settings screen

--- src/screens/Settings/SettingsScreen.js
+++ src/screens/Settings/SettingsScreen.js
@@ -1,17 +1,28 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 import { globalStyles } from '../../styles/global';
+import NetInfo, { NetInfoState } from "@react-native-community/netinfo";
 import { FontAwesome } from '@expo/vector-icons';
-import { Asset } from 'expo-asset';
-import * as FileSystem from 'expo-file-system';
-
-const showMyDBPath = async () => {
-  var mycmd = await FileSystem.getInfoAsync(FileSystem.documentDirectory);
-  console.log(mycmd);
-  Alert.alert('DB Path', JSON.stringify(mycmd));
-}
+import { Picker } from '@react-native-picker/picker';
 
 const SettingsScreen = () => {
+  const [selectedTheme, setSelectedTheme] = useState('default');
+  const [selectedLanguage, setSelectedLanguage] = useState('en-us');
+  const [isOnline, setIsOnline] = useState(false);
+
+  useEffect(() => {
+    const removeNetInfoSubscription = NetInfo.addEventListener((state: NetInfoState) => {
+      const online = (state.isConnected && state.isInternetReachable);
+      setIsOnline(online)
+    });
+
+    return () => removeNetInfoSubscription();
+  }, []);
+
+  const checkTheNetwork = () => {
+    Alert.alert("Network status", "" + isOnline);
+  }
+
   return (
     <View style={globalStyles.settings}>
       <View style={styles.settingsContent}>
@@ -19,8 +30,38 @@ const SettingsScreen = () => {
           <FontAwesome name="gear" size={20} />
           <Text style={styles.title}>Settings</Text>
         </View>
-        <Button title='Show my DB path' onPress={showMyDBPath} style={styles.btn} />
-        <Text style={styles.text}>In construction...</Text>
+        <View style={styles.optionsContainer}>
+          <Text style={styles.lblTitle}>Theme:</Text>
+          <Picker
+            selectedValue={selectedTheme}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedTheme(itemValue)
+            }
+            placeholder='Choose the theme'
+            style={styles.picker}>
+            <Picker.Item label="Choose the theme" value="" enabled={false} />
+            <Picker.Item label="Default" value="default" />
+            <Picker.Item label="Dark" value="dark" />
+            <Picker.Item label="Light" value="light" />
+          </Picker>
+        </View>
+        <View style={styles.optionsContainer}>
+          <Text style={styles.lblTitle}>Language:</Text>
+          <Picker
+            selectedValue={selectedLanguage}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedLanguage(itemValue)
+            }
+            placeholder='Choose the language'
+            style={styles.picker}>
+            <Picker.Item label="Choose the language" value="" enabled={false} />
+            <Picker.Item label="English (United States)" value="en-us" />
+            <Picker.Item label="Português (Portugal)" value="pt-pt" />
+          </Picker>
+        </View>
+        <View style={styles.optionsContentBtn}>
+          <Button title='Check the network status' onPress={checkTheNetwork} style={styles.btn} />
+        </View>
       </View>
     </View>
   );
@@ -41,8 +82,37 @@ const styles = StyleSheet.create({
     margin: 15,
     textAlign: 'center'
   },
+  optionsContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    margin: 15,
+    padding: 15,
+  },
+  lblTitle: {
+    textAlign: 'left',
+    justifyContent: 'flex-start',
+    color: 'black'
+  },
+  picker: {
+    justifyContent: 'flex-end',
+    backgroundColor: 'white',
+    color: 'black',
+    width: 230,
+    borderRadius: 15,
+  },
+  optionsContentBtn: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 15
+  },
   btn: {
     padding: 15,
+    margin: 15,
     borderRadius: 15,
     textAlign: 'center'
   }
