8713ef138959b4580b3463c0360bdd20a40af8db
Luigi Carvalho <carvalholuigi25@gmail.com>
2024-10-18 11:12:23 +0100

    fixed gensslcert.sh and added hotreloadenabled false to non-watch profiles

--- server/api/LCPMobileAppApi/SSL/gensslcert.sh
+++ server/api/LCPMobileAppApi/SSL/gensslcert.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SERVERNAME="localhost"
+NUMDAYS=$((365 * 10))
 
 if [ ! -d "Keys" ]; then
     mkdir -p "Keys"
@@ -10,5 +11,4 @@ if [ ! -d "Certs" ]; then
     mkdir -p "Certs"
 fi
 
-openssl req -x509 -newkey rsa:4096 -sha256 -days 365 -nodes -keyout Keys/$SERVERNAME.key -out Certs/$SERVERNAME.crt -subj "//CN=${SERVERNAME}" -extensions v3_ca -extensions v3_req -config config.cnf
-openssl pkcs12 -export -out Certs/$SERVERNAME.pfx -inkey Keys/$SERVERNAME.key -in Certs/$SERVERNAME.crt
+openssl req -x509 -newkey rsa:4096 -sha256 -days $NUMDAYS -nodes -keyout Keys/$SERVERNAME.key -out Certs/$SERVERNAME.crt -subj "//CN=${SERVERNAME}" -extensions v3_ca -extensions v3_req -config config.cnf && openssl pkcs12 -export -out Certs/$SERVERNAME.pfx -inkey Keys/$SERVERNAME.key -in Certs/$SERVERNAME.crt
